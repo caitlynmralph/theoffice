f19368e5c93d676f9b8405d0aeb318aab5db98b3
caitlynralph <caitlyn.ralph14@ncf.edu>
2017-07-06 19:03:08 -0400

    added new thats what she said design

---
+++ thatswhatshesaid.js
@@ -0,0 +1,38 @@
+var width = 600;
+var height = 400;
+var padding = 60;
+
+//Create svg element
+var div1 = d3.select(".thats-what-she-said-container")
+
+d3.tsv("spreadsheets/thatswhatshesaid_cleaned.tsv", function(data) {
+  data.forEach(function(d) {
+    d.Number = +d.Number;
+    console.log(d);
+  });
+
+  var block = div1
+    .append("p")
+    .selectAll("span")
+    .data(data)
+    .enter()
+    .append("span")
+    .attr("class",function(d){
+      if(d.Number==1){
+        return "season-episode-name";
+      }
+      else if(d.Number==2){
+        return "character-name"
+      }
+    })
+    .text(function(d){
+      if (d.Number == 1){
+        return d.SeasonEpisode + " ";
+      }
+      else if (d.Number == 2){
+        return d.Character;
+      }
+      else {return ": " + d.Line + " ";
+    }});
+
+});
