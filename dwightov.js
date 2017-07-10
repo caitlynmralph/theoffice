049e2634a9a30ee88c354b40d0d313b861a22467
caitlynralph <caitlyn.ralph14@ncf.edu>
2017-07-09 23:07:50 -0400

    add button generator to dwightov

--- dwightov.js
+++ dwightov.js
@@ -1,6 +1,7 @@
 var wi = 600;
 var he = 500;
 var padding = 50;
+var transitionDuration = 1000;
 
 var div3 = d3.select(".dwightov-container")
   .append("div");
@@ -19,44 +20,69 @@ d3.tsv("spreadsheets/dwight_markov_tweets.tsv", function(d, i, columns) {
   }, function(error, dataset) {
     if (error) throw error;
 
-    var svg3 = div3
-        .append("svg")
-        .attr("width",wi)
-        .attr("height",he);
+    console.log(dataset[getRandomInt(0,dataset.length)].line);
 
-    var response = svg3
-        .append("g");
+    document.getElementById("generate").onclick = function() {
+      document.getElementById("response").innerHTML = dataset[getRandomInt(0,dataset.length)].line;
+    }
 
-    var button = svg3
-        .append("rect")
-        .attr("x", wi-((padding*2)+500))
-        .attr("y",padding/2+10)
-        .attr("rx", 6)
-        .attr("ry", 6)
-        .attr("width",90)
-        .attr("height",35)
-        .attr("class","button");
-
-    var buttontext = svg3
-        .append("g");
-
-    buttontext.append("text")
-        .attr("x", wi-((padding*2)+493))
-        .attr("y",padding/2+32)
-        .attr("width",90)
-        .attr("height",35)
-        .attr("class","button-text")
-        .text("GENERATE")
-        .on('click', function() {
-          response.append("text")
-            .attr("x", wi-((padding*2)+500))
-            .attr("y",padding/2+80)
-            .attr("width",600)
-            .attr("height",40)
-            .attr("class","response-text")
-            .style("background-color","white")
-            .text(dataset[getRandomInt(0,dataset.length)].line);
-        });
+    // function redrawText() {
+    //     response
+    //       .attr("x", wi-((padding*2)+500))
+    //       .attr("y",padding/2+80)
+    //       .attr("width",600)
+    //       .attr("height",40)
+    //       // .attr("class","response-text")
+    //       .transition().duration(transitionDuration/2)
+    //       .style("color", "transparent")
+    //       .transition().duration(transitionDuration/2)
+    //       .style("color", "black")
+    //       .text(function(d) { return dataset[getRandomInt(0,dataset.length)].line});
+    // }
+    //
+    // var svg3 = div3
+    //     .append("svg")
+    //     .attr("width",wi)
+    //     .attr("height",he);
+    //
+    // var response = svg3
+    //     .append("g");
+    //
+    // var button = svg3
+    //     .append("rect")
+    //     .attr("x", wi-((padding*2)+500))
+    //     .attr("y",padding/2+10)
+    //     .attr("rx", 6)
+    //     .attr("ry", 6)
+    //     .attr("width",90)
+    //     .attr("height",35)
+    //     .attr("class","button");
+    //
+    // var buttontext = svg3
+    //     .append("g");
+    //
+    // var count = 0;
+    //
+    // response.append("text")
+    //   .attr("x", wi-((padding*2)+500))
+    //   .attr("y",padding/2+80)
+    //   .attr("width",600)
+    //   .attr("height",40)
+    //   // .attr("class","response-text")
+    //   .transition().duration(transitionDuration/2)
+    //   .style("color", "black")
+    //   .text(function(d) { return dataset[getRandomInt(0,dataset.length)].line});
+    //
+    // buttontext.append("text")
+    //     .attr("x", wi-((padding*2)+493))
+    //     .attr("y",padding/2+32)
+    //     .attr("width",90)
+    //     .attr("height",35)
+    //     .attr("class","button-text")
+    //     .text("GENERATE")
+    //     .on("click", function() {
+    //         redrawText();
+    //       });
 
 
     // var block = div3
