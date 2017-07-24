b637d3df899b17748bdeec294817e874562f8a9c
caitlynralph <caitlyn.ralph14@ncf.edu>
2017-07-24 14:53:42 -0400

    edited markov

--- markov/dwight_markov.py
+++ markov/dwight_markov.py
@@ -1,34 +1,27 @@
+#import packages
 import markovify
-import sys
 import csv
 import numpy as np
 import os
 
+#change working directory
 os.chdir('/Applications/MAMP/htdocs/theoffice/spreadsheets')
 
+#create CSV file to write to and open it
 with open('/Applications/MAMP/htdocs/theoffice/spreadsheets/dwight_markov_tweets.tsv', 'a') as csvfile:
 
+    #csv writer
     writer = csv.writer(csvfile, delimiter='\t',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
-    lines = np.zeros([0,0], dtype=object)
-
-    # Get raw text as string.
+    #upload corpus of text
     with open("dwight_lines.txt") as f:
         text = f.read()
 
-    # Build the model.
+    #build the markovify model
     text_model = markovify.NewlineText(text)
 
-    # # Print five randomly-generated sentences
-    # for i in range(5):
-    #     print(text_model.make_sentence())
-    #     text_model.make_sentence()
-
-    # Print three randomly-generated sentences of no more than 140 characters
+    #generate the sentences and write to CSV file
     for i in range(1000):
         print(text_model.make_short_sentence(140))
-        lines = np.append(lines,text_model.make_short_sentence(140))
-
-    for row in lines:
-        writer.writerow([row])
+        writer.writerow(text_model.make_short_sentence(140))
